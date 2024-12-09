bf508945cf6223614cefc53a3c2068d411bd9d3e
KS-HTK <2981026+ks-htk@users.noreply.github.com>
2024-12-09 10:49:11 +0100

    improved day09

--- day09/day09.py
+++ day09/day09.py
@@ -11,83 +11,85 @@ def profiler(method):
         return ret
     return profiler_method
 
-def calc_result(fs_list):
+def calc_result(fs: list[tuple[int, int]]):
     res = 0
-    for i, f_id in enumerate(fs_list):
-        if f_id == '.':
-            return res
-        res += f_id * i
+    index = 0
+    for f, l in fs:
+        if f != -1:
+            start = index
+            index += l
+            res += f * (index * (index - 1) - start * (start - 1)) // 2
+        else:
+            index += l
+    return res
 
 # Part 1:
-def part1(content = None) -> int:
-    file = True
-    f_id = 0
-    fs = []
-    for num in content[0]:
-        num = int(num)
-        fs.extend([f_id if file else '.'] * num)
-        if file:
-            f_id += 1
-        file = not file
+def part1(fs: list[tuple[int, int]]):
+    fs = fs.copy()
     last_num_ind = len(fs) - 1
-    for i in range(len(fs)):
-        if fs[i] == '.':
-            while fs[last_num_ind] == '.':
+    while True:
+        no_swap = True
+        for i, (f_id, l) in enumerate(fs):
+            if f_id != -1:
+                continue
+            while fs[last_num_ind][0] == -1:
                 last_num_ind -= 1
-            if last_num_ind < i:
-                return calc_result(fs)
-            fs[i] = fs[last_num_ind]
-            fs[last_num_ind] = '.'
+            if last_num_ind <= i:
+                break
+            no_swap = False
+            file_id, file_len = fs[last_num_ind]
+            if file_len > l:
+                fs[last_num_ind] = (file_id, file_len - l)
+                fs[i] = (file_id, l)
+            elif file_len < l:
+                del fs[last_num_ind]
+                fs[i] = (file_id, file_len)
+                fs.insert(i + 1, (-1, l - file_len))
+                break
+            else:
+                del fs[last_num_ind]
+                fs[i] = (file_id, file_len)
+        if no_swap:
+            break
     return calc_result(fs)
 
-def calc_result2(fs_list):
-    expanded_fs_list = []
-    for f_id, num in fs_list:
-        expanded_fs_list.extend([f_id] * num)
-    checksum = []
-    for i, f_id in enumerate(expanded_fs_list):
+# Part 2:
+def part2(fs: list[tuple[int, int]]):
+    free_spaces = []
+    for ind, (f_id, length) in enumerate(fs):
+        if f_id == -1:
+            free_spaces.append((ind, length))
+
+    for i in range(len(fs) - 1, -1, -1):
+        f_id, length = fs[i]
         if f_id == -1:
             continue
-        checksum.append((f_id, i))
-    return sum([a*b for a, b in checksum])
+        for j, (index, free_length) in enumerate(free_spaces):
+            if free_length >= length and index < i:
+                fs[index] = (f_id, length)
+                fs[i] = (-1, length)
+                remaining_space = free_length - length
+                if remaining_space > 0:
+                    free_spaces[j] = (index, remaining_space)
+                    fs.insert(index+1, (-1, remaining_space))
+                    free_spaces = [(k+1 if k >= index else k, l) for k, l in free_spaces]
+                else:
+                    free_spaces.pop(j)
+                break
+    return calc_result(fs)
 
-# Part 2: > 264203879949
-def part2(content = None) -> int:
+def get_input():
+    with open(os.path.dirname(os.path.realpath(__file__))+'/input', 'r', encoding='utf-8') as f:
+        content = [s.strip() for s in f.read().rstrip().split('\n')]
     file = True
     f_id = 0
     fs: list[tuple[int, int]] = []
     for num in content[0]:
         num = int(num)
         fs.append((f_id, num) if file else (-1, num))
-        if file:
-            f_id += 1
+        f_id += file
         file = not file
-
-# Part 2:
-    while True:
-        no_swap = True
-        for i in [i for i, (j, _) in enumerate(fs) if j == -1]:
-            last_num_ind = len(fs) - 1
-            while 0 <= i <= last_num_ind and (fs[last_num_ind][0] == -1 or fs[last_num_ind][1] > fs[i][1]):
-                last_num_ind -= 1
-            if last_num_ind < i:
-                continue
-            no_swap = False
-            free_space = fs[i][1]  # store length of free space
-            file_to_swap = fs[last_num_ind]
-            fs[i] = file_to_swap
-            fs[last_num_ind] = (-1, file_to_swap[1])
-            if free_space > file_to_swap[1]:
-                fs.insert(i+1, (-1, free_space-file_to_swap[1]))
-                break
-        if no_swap:
-            break
-    return calc_result2(fs)
-
-def get_input():
-    with open(os.path.dirname(os.path.realpath(__file__))+'/input', 'r', encoding='utf-8') as f:
-        content = [s.strip() for s in f.read().rstrip().split('\n')]
-    return content
+    return fs
 
 @profiler
 def solve():
